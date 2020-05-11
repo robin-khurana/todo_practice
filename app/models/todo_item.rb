f29be8fdbe5b5fe67b7d380146eab3d2fa8642bf
robin-khurana <robin@interviewbit.com>
2020-05-12 02:42:59 +0530

    Added the feature

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,7 +1,35 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
 
+  after_create :assign_status
+
+  enum status: {red: 0, yellow: 1, blue: 2, lightgreen: 3}
+
+  default_scope { order(due_date: :asc) }
+
   def completed?
     !completed_at.blank?
   end
+
+  def item_complete_updation
+    self.update!(completed_at: Time.now, status: :lightgreen, due_date: close_to_infinite)
+  end
+
+  def assign_status
+    if self.completed?
+      self.lightgreen!
+    elsif self.due_date <= Time.now.utc
+      self.red!
+    elsif self.due_date <= Time.now.utc + 2.days
+      self.yellow!
+    else
+      self.blue!
+    end
+  end
+
+  private
+  def close_to_infinite
+    Date.today + 500.years
+  end
+
 end
